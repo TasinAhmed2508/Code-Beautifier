a4fd63464677092d7dd8d3cf276807484002b167
Tasin Ahmed <tasinahmed2508@gmail.com>
2026-01-18 22:09:18 +0600

    Layout

--- js/features/navbar-morph.js
+++ js/features/navbar-morph.js
@@ -1,15 +1,3 @@
 export const bindNavbarMorph = () => {
-  const navbar = document.getElementById('navbar');
-  if (!navbar) return;
-
-  const onScroll = () => {
-    if (window.scrollY > 20) {
-      navbar.classList.add('is-floating');
-    } else {
-      navbar.classList.remove('is-floating');
-    }
-  };
-
-  onScroll();
-  window.addEventListener('scroll', onScroll, { passive: true });
+  return;
 };
